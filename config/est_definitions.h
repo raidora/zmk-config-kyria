663d6c60bcf1b684b2b5de626234cba17322742e
Raido Oras <oras.raido@gmail.com>
2023-08-13 11:52:14 +0300

    Baseline keymap

---
+++ config/est_definitions.h
@@ -0,0 +1,78 @@
+//Special characters for estonian keyboard layout
+#define EST_DOTTED_O                 &kp SEMICOLON 
+#define EST_ROOF_O                   &kp RIGHT_BRACKET
+#define EST_DOTTED_A                 &kp APOSTROPHE
+#define EST_DOTTED_U                 &kp LEFT_BRACKET
+#define EST_DASH                     &kp SLASH
+#define EST_LEFT_ANGLE_BRACKET       &kp NUBS
+#define EST_RIGHT_ANGLE_BRACKET      &kp LS(NUBS)
+#define EST_LEFT_BRACKET             &kp LS(N8)
+#define EST_RIGHT_BRACKET            &kp LS(N9)
+#define EST_LEFT_CURLY_BRACKET       &kp RA(N7)
+#define EST_RIGHT_CURLY_BRACKET      &kp RA(N0)
+#define EST_LEFT_SQUARE_BRACKET      &kp RA(N8)
+#define EST_RIGHT_SQUARE_BRACKET     &kp RA(N9)
+
+#define EST_DOLLAR                   &kp RA(N4)
+#define EST_AMPERSAND                &kp LS(N6)
+#define EST_EQUALS                   &kp LS(N0)
+#define EST_UNDERSCORE               &kp LS(SLASH)
+#define EST_DOUBLE_QUOTE             &kp LS(N2)
+#define EST_SINGLE_QUOTE             &kp BACKSLASH
+#define EST_PIPE                     &kp RA(NUBS)
+#define EST_MINUS                    &kp SLASH
+#define EST_PLUS                     &kp MINUS
+#define EST_CARET                    &kp RA(APOS)
+#define EST_BACKTICK                 &kp LS(EQUAL)
+#define EST_HASHTAG                  &kp LS(N3)
+#define EST_QUESTION_MARK            &kp LS(MINUS)
+#define EST_SEMI_COLON               &kp LS(COMMA)
+#define EST_COLON                    &kp LS(PERIOD)
+#define EST_EXCLAMATION_MARK         &kp LS(N1)
+#define EST_ASTERISK                 &kp LS(BACKSLASH)
+#define EST_BACKSLASH                &kp RA(MINUS)
+#define EST_FORWARDSLASH             &kp LS(N7)
+#define EST_COMMERCIAL_AT            &kp RA(N2)
+#define EST_PERCENT                  &kp LS(N5)
+#define EST_TILDE                    &kp LS(GRAVE)
+#define EST_CARON                    &kp GRAVE
+#define EST_EURO                     &kp RA(N5)
+
+// Shorthands
+#define EST_DOTDO                    EST_DOTTED_O
+#define EST_ROOFO                    EST_ROOF_O
+#define EST_DOTDA                    EST_DOTTED_A
+#define EST_DOTDU                    EST_DOTTED_U
+
+// Brackets
+#define EST_LABR                     EST_LEFT_ANGLE_BRACKET
+#define EST_RABR                     EST_RIGHT_ANGLE_BRACKET
+#define EST_LCBR                     EST_LEFT_CURLY_BRACKET
+#define EST_RCBR                     EST_RIGHT_CURLY_BRACKET
+#define EST_LSBR                     EST_LEFT_SQUARE_BRACKET
+#define EST_RSBR                     EST_RIGHT_SQUARE_BRACKET
+#define EST_LBRC                     EST_LEFT_BRACKET
+#define EST_RBRC                     EST_RIGHT_BRACKET
+
+// Symbols
+#define EST_SCLN                     EST_SEMI_COLON
+#define EST_COLN                     EST_COLON
+#define EST_USCR                     EST_UNDERSCORE
+#define EST_QSTN                     EST_QUESTION_MARK
+#define EST_BKTK                     EST_BACKTICK
+#define EST_HSTG                     EST_HASHTAG
+#define EST_EQLS                     EST_EQUALS
+#define EST_AMPR                     EST_AMPERSAND
+#define EST_MINS                     EST_MINUS
+#define EST_DOLR                     EST_DOLLAR
+#define EST_EXCL                     EST_EXCLAMATION_MARK
+#define EST_ASTR                     EST_ASTERISK
+#define EST_BSLS                     EST_BACKSLASH
+#define EST_SLSH                     EST_FORWARDSLASH
+#define EST_DBQT                     EST_DOUBLE_QUOTE
+#define EST_SIQT                     EST_SINGLE_QUOTE
+#define EST_ATSI                     EST_COMMERCIAL_AT
+#define EST_PRCT                     EST_PERCENT
+#define EST_TLDE                     EST_TILDE
+#define EST_CARO                     EST_CARON
+#define EST_CARE                     EST_CARET
---
+++ config/key_shorthands.h
@@ -0,0 +1,4 @@
+#define VOLMUTE    &kp C_MUTE
+#define VOLDOWN    &kp C_VOLUME_DOWN
+#define VOLUP      &kp C_VOLUME_UP
+#define PLAYPAUS   &kp C_PLAY
